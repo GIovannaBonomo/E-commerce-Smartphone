17f8e5b97e52b1fb7eb6e60cf58e6de61b90d525
FrancescaBonomo <francybonomo@msn.com>
2025-04-11 17:09:52 +0200

    .

---
+++ e-commerce.js
@@ -0,0 +1,4 @@
+//let titolo = document.getElementById("nomeNegozio").textContent = "La tua bellezza la nostra passione"
+//let pagina = document.body.style.backgroundColor= "#fff2f2"
+//let indirizzoModificato = document.getElementsByClassName(indirizzo).innerText= "Via Grimaldi 10 Ragusa"
+//document.getElementsByClassName("indirizzo")[0].innerHTML = `<p style= "color: white; text-align: center; margin: 0; font-size: 10px;">Via Grimaldi 10 <br> Roma</p>`
