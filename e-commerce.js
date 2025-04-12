f0ffcfbe5bc4ee8d40fea42258ba9cff99be3d39
GIovannaBonomo <francybonomo@msn.com>
2025-04-12 16:47:27 +0200

    .

--- e-commerce.js
+++ e-commerce.js
@@ -1,4 +1,54 @@
-//let titolo = document.getElementById("nomeNegozio").textContent = "La tua bellezza la nostra passione"
-//let pagina = document.body.style.backgroundColor= "#fff2f2"
-//let indirizzoModificato = document.getElementsByClassName(indirizzo).innerText= "Via Grimaldi 10 Ragusa"
-//document.getElementsByClassName("indirizzo")[0].innerHTML = `<p style= "color: white; text-align: center; margin: 0; font-size: 10px;">Via Grimaldi 10 <br> Roma</p>`
+ let titolo = document.getElementById("nomeNegozio")
+ let titoloOriginale = "Luce di Bellezza"
+ let titoloModificato = "La tua bellezza la nostra passione"
+    titolo.onclick = function(){
+    if (titolo.innerHTML === titoloOriginale){
+        titolo.innerHTML = titoloModificato
+    }else {
+        titolo.innerText = titoloOriginale
+    }
+    }
+let pagina = document.querySelector("body")
+let paginaOriginale = "white"
+let paginaModificata = "#ffeff1"
+    pagina.onmouseenter = function(){
+        if ( pagina.style.backgroundColor === paginaOriginale ){
+            pagina.style.backgroundColor = paginaModificata
+        }else {
+            pagina.style.backgroundColor = paginaOriginale
+        }
+    }
+let indirizzo = document.getElementsByClassName("indirizzo")[0]
+let indirizzoOriginale= "Via Giacomo Matteotti 2 <br> Modica"
+let indirizzoModificato = "Via Grimaldi 10 <br> Ragusa"
+    indirizzo.onclick= function(){
+        if (indirizzo.innerHTML === indirizzoOriginale){
+            indirizzo.innerHTML = indirizzoModificato
+        }else{
+            indirizzo.innerHTML = indirizzoOriginale
+        }
+    }
+    
+function cambiaColore(){
+    let link = document.querySelectorAll("a")
+     for (let a of link){
+         a.classList.add("link")
+         console.log ("true")
+     }
+    }
+
+function rimuoviImg(){
+    let image = document.querySelectorAll("img")
+     for (let img of image){
+         img.classList.add("image")
+         console.log ("ho aggiunto la classe",img)
+     }
+    }
+
+function colorePrezzo(){
+    let prezzoRosso= document.getElementsByClassName("prezzo")
+        for( let prezzo of prezzoRosso){
+            prezzo.classList.add("prezzoRosso")
+        }
+}
+    
